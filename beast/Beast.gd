7eb1e3416846debb13d8f18ee414ff49e7fbfb4c
jackflower <jackflower@poczta.onet.pl>
2018-11-16 10:24:08 +0100

    Update
    Poprawiony skrypt dla sceny Beast

--- beast/Beast.gd
+++ beast/Beast.gd
@@ -7,16 +7,20 @@ var ilosc_klatek = 0
 
 func _ready():
 	ilosc_klatek = $Sprite.hframes * $Sprite.vframes
+	$Sprite.frame = 0
 	pass
 
 
 func _on_Timer_timeout():
-	#klatka = klatka +1
-	klatka += 1
-	#print ($Sprite.frame)
+	# klatka = klatka + 1
+	klatka += 1           
 	
 	if(klatka >= ilosc_klatek):
 		klatka = 0
 	$Sprite.frame = klatka
+	
+	#print ($Sprite.frame)
+	
 	pass
+	
 	
